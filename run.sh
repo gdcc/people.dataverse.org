9a472db542558d7343a42e65aec9e6e4a722e363
Philip Durbin <philipdurbin@gmail.com>
2026-04-06 09:30:33 -0400

    get routes working after DNS switch
    We've switched to https://people.dataverse.org
    from https://gdcc.github.io/people.dataverse.org/

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 docker run -p 8080:80 \
   -v ./nginx.conf:/etc/nginx/nginx.conf \
   -v ./404.html:/usr/share/nginx/html/404.html \
-  -v "$(pwd)/../:/usr/share/nginx/html" \
+  -v "$(pwd)/:/usr/share/nginx/html" \
   nginx:latest
